4aafd9fef7dd30a7ab10996d8cd0241b340898a5
mauliddina <109194349+mauliddina@users.noreply.github.com>
2022-11-24 08:23:48 +0700

    Add files via upload

--- hewan.php
+++ hewan.php
@@ -3,48 +3,46 @@
 </head>
 <center>
 <body>
-<?php
-session_start();
-if($_SESSION['status']!="login"){
-    header("location:../index.php?pesan=belum_login");
-}
-?>
+    <?php
+    session_start();
 
-<h4>Selamat Datang, <?php echo $_SESSION['username'];?>! Anda Telah Login.</h4>
-    <h1>Pemeriksaan Hewan Di RS Sumber Waras</h1>
+    if($_SESSION['status']!="login"){
+        header("location:../index.php?pesan=belum_login");
+    }
+?>
+<h4>Selamat Datang, <?php echo $_SESSION['username'];?>! anda telah login.</h4>
+    <h1>from pemeliharaan hewan sumber waras</h1>
     <form action="hewan.php" method="get">
-        <label> cari:</label>
-        <input type="text" name="cari">
-        <nput type="submit" value="cari">
+	<label>Cari :</label>
+	<input type="text" name="cari">
+	<input type="submit" value="Cari">
 </form>
 
-<?php
+<?php 
 if(isset($_GET['cari'])){
-    echo"<b>Hasil Pencarian:" $cari "</b>";
+	$cari = $_GET['cari'];
+	echo "<b>Hasil pencarian : ".$cari."</b>";
 }
 ?>
-
-    <h4><a href="logout.php">LOGOUT</a>
-</h4>
+    <h4><a href="logout.php">LOGOUT</a></h4>
     <table border="1">
         <tr>
             <th>id</th>
             <th>Nama Pemilik</th>
-            <th>Jenis Hewan</th>
-            <th>Keluhan</th>
+            <th>jenis hewan</th>
+            <th>keluhan</th>
         </tr>
+
 <?php
 include("koneksi.php");
 if(isset($_GET['cari'])){
     $cari=$_GET['cari'];
-    $sql="SELECT * FROM tb_pemeriksaan WHERE nama LIKE '%".$cari."%'";
-    $query=mysqli_query($koneksi, $sql);
-}else{
-    $sql= 'SELECT * FROM tb_pemeriksaan';
+    $sql="SELECT * FROM tb_pemeriksaan WHERE nama LIKE '%" .$cari."%'";
     $query=mysqli_query($koneksi,$sql);
+}else{
+$sql='SELECT * FROM tb_pemeriksaan';
+$query=mysqli_query($koneksi,$sql);
 }
-
-
 while($db_pemeriksaanhewan=mysqli_fetch_array($query)){
     echo"<tr>";
     echo"<td>".$db_pemeriksaanhewan['id']."</td>";
@@ -52,16 +50,15 @@ while($db_pemeriksaanhewan=mysqli_fetch_array($query)){
     echo"<td>".$db_pemeriksaanhewan['jh']."</td>";
     echo"<td>".$db_pemeriksaanhewan['keluhan']."</td>";
     echo"<td>";
-    echo "<a href='edit.php?id=".$db_pemeriksaanhewan['id']."'>Edit</a>|";
-    echo "<a href='hapus.php?id=".$db_pemeriksaanhewan['id']."'>Hapus</a>";
+    echo "<a href='edit.php?id=".$db_pemeriksaanhewan['id']."'>Edit</a> |";
+    echo "<a href='hapus.php?id=".$db_pemeriksaanhewan['id']."'>Hapus</a> |";
+    
     echo"</td>";
     echo"</tr>";
 }
-
 ?>
-</center>
 </table>
-
-<h4><a href="tambah.php"><input type="submit" value="Tambah Data" name="tambah"/></a></h4>
+<h4><a href ="tambah.php"><input type="submit" value="tambah" name="tambah"/></h4></a>
+</center>
 </body>
 </html>
